33fdcc86e2f683e91e49fbf683fecbe19831df43
Ogushanakin <ogzhnakn96@gmail.com>
2022-10-26 01:43:08 +0300

    LoginScreen-UI
    Login Screen UI created and app logo changed.

---
+++ SHOPPING-APP/SHOPPING-APP/Screens/LoginScreen/LoginViewController.swift
@@ -0,0 +1,154 @@
+//
+//  MainController.swift
+//  SHOPPING-APP
+//
+//  Created by AKIN on 25.10.2022.
+//
+
+import UIKit
+
+protocol AuthenticationControllerProtocol {
+    func checkFormStatus()
+}
+
+protocol AuthenticationDelegate: AnyObject {
+    func authenticationComplete()
+}
+
+final class LoginViewController: UIViewController,
+                                 OnboardingSeenProtocol{
+    
+    // MARK: - Properties
+    
+    private var viewModel = LoginViewModel()
+    
+    weak var delegate: AuthenticationDelegate?
+    
+    var isOnboardingSeen = true
+    
+    private let iconImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "logo_transparent")
+        return iv
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        return InputContainerView(image: UIImage(systemName: "envelope"),
+                                  textField: emailTextField)
+    }()
+    
+    private lazy var passwordContainerView: InputContainerView = {
+        return InputContainerView(image: UIImage(systemName: "lock"),
+                                  textField: passwordTextField)
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.tintColor = .white
+        button.backgroundColor = #colorLiteral(red: 0.1220499948, green: 0.1906306446, blue: 0.2015277445, alpha: 0.6343905215)
+        button.setHeight(height: 50)
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    private let emailTextField = CustomTextField(placeholder: "Email")
+    
+    private let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private lazy var dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ",
+                                                        attributes: [.font: UIFont.systemFont(ofSize: 16),
+                                                                     .foregroundColor: UIColor.white])
+        attributedTitle.append(NSAttributedString(string: "Sign Up",
+                                                  attributes: [.font: UIFont.boldSystemFont(ofSize: 16),
+                                                               .foregroundColor: UIColor.white]))
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleLogin() {
+        print("login")
+    }
+    
+    @objc func handleShowSignUp() {
+        print("signup")
+        
+        checkFormStatus()
+    }
+    
+    @objc func textDidChange(sender: UITextField) {
+        if sender == emailTextField {
+            viewModel.email = sender.text
+        } else {
+            viewModel.password = sender.text
+        }
+        
+        checkFormStatus()
+    }
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        configureGradientBackground()
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        iconImage.setDimensions(height: 250, width: 300)
+         
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                     paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor,
+                                     bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                     right: view.rightAnchor, paddingLeft: 32, paddingRight: 32)
+        
+        emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+   
+}
+
+extension LoginViewController: AuthenticationControllerProtocol {
+    
+    func checkFormStatus() {
+        if viewModel.formIsValid {
+            loginButton.isEnabled = true
+            loginButton.backgroundColor = #colorLiteral(red: 0.1220499948, green: 0.1906306446, blue: 0.2015277445, alpha: 1)
+        } else {
+            loginButton.isEnabled = false
+            loginButton.backgroundColor = #colorLiteral(red: 0.1220499948, green: 0.1906306446, blue: 0.2015277445, alpha: 0.6332005381)
+        }
+    }
+}
