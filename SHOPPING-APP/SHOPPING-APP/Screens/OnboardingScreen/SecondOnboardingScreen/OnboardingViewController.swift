5b810ad12f9ff7e13ca080abc0a9dbd4c2ee31a7
Ogushanakin <ogzhnakn96@gmail.com>
2022-10-25 10:42:29 +0300

    WelcomeScreen/ FirstOnboardingScreen Added
    On this screen used AVPlayer.

---
+++ SHOPPING-APP/SHOPPING-APP/Screens/OnboardingScreen/SecondOnboardingScreen/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingViewController.swift
+//  SHOPPING-APP
+//
+//  Created by AKIN on 25.10.2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
