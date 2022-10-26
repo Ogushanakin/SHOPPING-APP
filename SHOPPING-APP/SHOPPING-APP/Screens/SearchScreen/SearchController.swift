de537f1dc7bf06fc2838129c5644806f1e8ab700
Ogushanakin <ogzhnakn96@gmail.com>
2022-10-26 11:08:57 +0300

    MainTabBar created
    Products, Search and Profile controllers added that Main Tab Bar

---
+++ SHOPPING-APP/SHOPPING-APP/Screens/SearchScreen/SearchController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchController.swift
+//  SHOPPING-APP
+//
+//  Created by AKIN on 26.10.2022.
+//
+
+import UIKit
+
+final class SearchController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
